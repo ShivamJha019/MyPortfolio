1e6084b577852864382595140374447825859d60
parshvanth.webapp.innovista <parshvanth.webapp.innovista@gmail.com>
2025-11-05 12:03:00 +0530

    exp updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -19,10 +19,10 @@ export function Hero() {
           <h1>Shivam Jha</h1>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
-          <h3>Full Stack Developer</h3>
+          <h3>Software Engineer</h3>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">5 Years of Experience</p>
+          <p className="small-resume">6 Years of Experience</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
           <BrowserRouter>
